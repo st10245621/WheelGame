b52ca89eda5fb61100f56f19ee39bd24cabd1a69
Gerard <104638929+st10245621@users.noreply.github.com>
2024-10-25 16:04:17 +0200

    removed changes

--- obj/Debug/net8.0-windows/WheelGame.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/WheelGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WheelGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e5c3da40c442afbc7fc99cdf6f0209f67a6d5d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+293d90401f39bc0750cbf2a3e5b05140919d4bc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WheelGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WheelGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
